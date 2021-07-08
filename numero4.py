9a2f7e91afbd2ab41638b72d19b0b28d7c0c2c7f
Francisco Bustamante <chicolucio@gmail.com>
2021-07-08 18:13:25 -0300

    Correcao do arquivo numero4.py. Ainda apenas para reais.

--- numero4.py
+++ numero4.py
@@ -1,7 +1,9 @@
-import numpy
+from math import sqrt
 
-raizes (a, b, c) = 
-delta = (b ** 2 - 4 * a * c) 
-x1 = (-b  + numpy.sqrt(delta) / 2 * a)
-x2 = (-b - numpy.sqrt(delta) / 2 * a)
+a, b, c = 1, 1, 0
+delta = b**2 - 4 * a * c
 
+x1 = (-b + sqrt(delta)) / (2 * a)
+x2 = (-b - sqrt(delta)) / (2 * a)
+
+print(x1, x2)
