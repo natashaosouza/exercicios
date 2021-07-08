bbd1c1b4c0c281aafb3da68f2c27d5664fa56eb2
Francisco Bustamante <chicolucio@gmail.com>
2021-07-08 17:50:49 -0300

    Correcao do arquivo numero3.py

--- numero3.py
+++ numero3.py
@@ -1,14 +1,9 @@
-import numpy 
+from math import log, sqrt, sin, e
 
-log_de_dois = numpy.log(2)  
-raiz_de_cinco = numpy.sqrt(5)
+letra_a = (2 + e**2.8) / (sqrt(13) - 2)
 
-letra_b = (1 - (1 + log_de_dois ** -3.5)) / 1 + raiz_de_cinco
+letra_b = (1 - pow(1 + log(2), -3.5)) / (1 + sqrt(5))
 
+letra_c = sin((2 - sqrt(2)) / (2 + sqrt(2)))
 
-numerador = 2 - numpy.sqrt(2)
-denominador = 2 + numpy.sqrt(2)
-
-letra_c = numpy.sin(numerador / denominador)
-
-print( letra_b , letra_c)
+print(letra_a, letra_b, letra_c)
