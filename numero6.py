3102b9edaddefed5700c3ef6d829459310ba991f
Francisco Bustamante <chicolucio@gmail.com>
2021-07-13 17:05:43 -0300

    Correcao do arquivo numero6.py

--- numero6.py
+++ numero6.py
@@ -1,5 +1,21 @@
+def velocidade_media(distancia, tempo):
+    """Calcula a velocidade media a partir de distancia e tempo.
 
-velocidade = "Calcula a velocidade media a partir de distancia e tempo."
+    Parâmetros:
+    ----------
+    distancia : int ou float
+    tempo : int ou float
 
-def velocidade(distancia, tempo):
+    Saída:
+    ------
+    velocidade media : float
+    """
     return distancia / tempo
+
+
+distancia = float(input("Forneça um valor de distância: "))
+tempo = float(input("Forneça um valor de tempo: "))
+
+resultado = velocidade_media(distancia, tempo)
+
+print(f"A velocidade média é {resultado:.3f}")
