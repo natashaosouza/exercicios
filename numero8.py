5a4cc0375dab1291a0bc4542ffcc9b12542a328e
Francisco Bustamante <chicolucio@gmail.com>
2021-07-15 16:55:11 -0300

    Correcao do numero8.py

--- numero8.py
+++ numero8.py
@@ -3,9 +3,10 @@ from math import pi, sin
 raio = 0.5
 lados = (3, 4, 5, 100, 10_000, 1_000_000)
 
-def func_1(pi, n):
-    return pi / n
+
+def perimetro(numero_de_lados):
+    return 2 * numero_de_lados * raio * sin(pi / numero_de_lados)
+
 
 for n in lados:
-    perimetro = 2 * n * raio * sin(func_1)
-    print(perimetro)
+    print(perimetro(n))
