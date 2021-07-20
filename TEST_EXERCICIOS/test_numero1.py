b8dd2295b7050eec037ce0b7403bdd578a99b528
Natasha Souza <natasha.oliveira98@gmail.com>
2021-07-20 10:48:07 -0300

    Teste do exercício 1

---
+++ TEST_EXERCICIOS/test_numero1.py
@@ -0,0 +1,26 @@
+from numero1 import altura, velocidade
+import pytest
+
+
+def test_altura_inicial_0_velocidade_inicial_0_tempo_1():
+    assert altura(0, 0, 1) == -4.9
+
+
+def test_altura_inicial_0_velocidade_inicial_1_tempo_1():
+    assert altura(0, 1, 1) == 4.9
+
+
+def test_altura_inicial_1_velocidade_inicial_0_tempo_2():
+    assert altura(1, 0, 2) == 19.6
+
+
+def test_velocidade_inicial_0_tempo_1():
+    assert velocidade(0, 1) == 9.8
+
+
+def test_velocidade_inicial_1_tempo_2():
+    assert velocidade(1, 2) == -18.6
+
+
+def test_velocidade_inicial_10_tempo_1():
+    assert velocidade(10, 1) == 0.2
