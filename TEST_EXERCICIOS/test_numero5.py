8514aa65ec16b1707e16edd9e4c9e36fb119d469
Natasha Souza <natasha.oliveira98@gmail.com>
2021-07-20 15:00:11 -0300

    Teste do exercício 5

---
+++ TEST_EXERCICIOS/test_numero5.py
@@ -0,0 +1,18 @@
+from numero8 import perimetro
+import pytest
+
+
+def test_numero_de_lados():
+    assert perimetro(3) == 2.59807621135
+
+
+def test_numero_de_lados():
+    assert perimetro(4) == 2.82842712475
+
+
+def test_numero_de_lados():
+    assert perimetro(5) == 2.93892626146
+
+
+def test_numero_de_lados():
+    assert perimetro(100) == 3.14107590781
