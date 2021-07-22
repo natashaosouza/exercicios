0df66edf31fb6101c4156462fc0e6e575f359d31
Francisco Bustamante <chicolucio@gmail.com>
2021-07-22 17:16:36 -0300

    Correcao do numero2.py e seus testes

--- numero2.py
+++ numero2.py
@@ -4,5 +4,9 @@ V0 = 10
 a = 2.5
 z = 4 * 1/3
 
-V = V0 * (1 - z / math.sqrt(a**2 + z**2))
-print(V)
+
+def V(V0, a, z):
+    return V0 * (1 - z / math.sqrt(a**2 + z**2))
+
+
+print(V(V0, a, z))
--- tests/test_numero2.py
+++ tests/test_numero2.py
@@ -1,13 +1,13 @@
 from numero2 import V
+import pytest
 
 
-def test_V0_1_a_1_z_1():
-    assert V(1, 1, 1) == 0.5
-
-
-def test_V0_1_a_1_z_2():
-    assert V(1, 1, 2) == 0.10
-
-
-def test_V0_1_a_2_z_2():
-    assert V(1, 2, 2) == 0.29
+@pytest.mark.parametrize(
+    "V0, a, z, esperado", (
+        (1, 1, 1, 0.292),
+        (1, 1, 2, 0.105),
+        (1, 2, 2, 0.292),
+    )
+)
+def test_V(V0, a, z, esperado):
+    assert V(V0, a, z) == pytest.approx(esperado, abs=1E-3)
