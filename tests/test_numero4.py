a1c086ee43faf59fb12ed98e76e53d833ac6d727
Francisco Bustamante <chicolucio@gmail.com>
2021-07-22 17:55:09 -0300

    Correcao do numero4 ou (7) e seus testes

--- tests/test_numero4.py
+++ tests/test_numero4.py
@@ -1,4 +1,5 @@
-from numero7_funcao import delta
+from numero7_funcao import delta, raizes
+from math import sqrt
 
 
 def test_delta_a_3_b_7_c_4():
@@ -19,3 +20,15 @@ def test_delta_a_4_b_10_c_2():
 
 def test_delta_a_5_b_8_c_5():
     assert delta(5, 8, 5) == -36
+
+
+def test_duas_raizes_1_1_0():
+    assert raizes(1, 1, 0) == (0, -1)
+
+
+def test_duas_raizes_1_2_1():
+    assert raizes(1, 2, 1) == (-1, -1)
+
+
+def test_raiz_complexa():
+    assert raizes(1, 1, 1) == (complex(-0.5, sqrt(3)/2), complex(-0.5, -sqrt(3)/2))
