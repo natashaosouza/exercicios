47184ef98656827a3a1f5763938c8149a5e5a32e
Francisco Bustamante <chicolucio@gmail.com>
2021-07-29 16:21:08 -0300

    correcao do parametrize do numero 5

--- tests/test_numero5.py
+++ tests/test_numero5.py
@@ -14,17 +14,17 @@ def test_perimetro(numero_de_lados, esperado):
     assert perimetro(numero_de_lados) == pytest.approx(esperado, abs=1E-3)
 
 
-def test_numero_de_lados():
-    assert perimetro(3) == 2.59807621135
+# def test_numero_de_lados_tres():
+#     assert perimetro(3) == 2.59807621135
 
 
-def test_numero_de_lados():
-    assert perimetro(4) == 2.82842712475
+# def test_numero_de_lados_quatro():
+#     assert perimetro(4) == 2.82842712475
 
 
-def test_numero_de_lados():
-    assert perimetro(5) == 2.93892626146
+# def test_numero_de_lados_cinco():
+#     assert perimetro(5) == 2.93892626146
 
 
-def test_numero_de_lados():
-    assert perimetro(100) == 3.14107590781
+# def test_numero_de_lados_cem():
+#     assert perimetro(100) == 3.14107590781
