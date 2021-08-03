15c75b9ea1a774a613a4fe7f37a037a79d2d2237
Natasha Souza <natasha.oliveira98@gmail.com>
2021-08-03 14:23:46 -0300

    Primeira versão do numero_1_pint

--- numero1.py
+++ numero1.py
@@ -1,14 +1,18 @@
 GRAVIDADE = 9.8   # m/s^2
+import pint
+
+ureg = pint.UnitRegistry()
 
 # altura_inicial = 1.6   # m
 # velocidade_inicial = 14.2   # m/s
 # tempo = 0.5  # s
 
-
+@ureg.wraps('m', ('m', 'm / s', 's'))
 def altura(altura_inicial, velocidade_inicial, tempo):
     return altura_inicial + velocidade_inicial * tempo - 1/2 * GRAVIDADE * tempo**2
 
 
+@ureg.wraps('m / s', ('m / s', 's'))
 def velocidade(velocidade_inicial, tempo):
     return velocidade_inicial - GRAVIDADE * tempo
 
