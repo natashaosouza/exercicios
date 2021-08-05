d771bdb8a31d35f75e488a75176089177da123cf
Natasha Souza <natasha.oliveira98@gmail.com>
2021-08-05 14:07:03 -0300

    Primeira versão do exercício com volume molar

---
+++ numero9.py
@@ -0,0 +1,24 @@
+import pint
+
+ureg = pint.UnitRegistry()
+Q_ = ureg.Quantity
+
+GAS_CONSTANT = Q_(0.082057366080960, 'atm * l / (mol * K)')
+
+
+@ureg.wraps('liter', ('mol', 'K', 'atm'))
+def volume_molar(mole_number, temperature, pressure):
+    """ Calcula o volume molar de um gás ideal
+
+    Parâmetros:
+    ----------
+    numero de mol
+    constante dos gases
+    temperatura
+    pressão
+
+    Saída:
+    -----
+    volume molar
+    """
+    return mole_number * GAS_CONSTANT.magnitude * temperature / pressure
