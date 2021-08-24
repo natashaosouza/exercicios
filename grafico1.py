8ea5036f90fac98055db01fd6135b05e1d643c95
Francisco Bustamante <chicolucio@gmail.com>
2021-08-24 18:05:33 -0300

    Primeira versao com classe

--- grafico1.py
+++ grafico1.py
@@ -10,54 +10,59 @@ ureg.setup_matplotlib()
 
 data_file = 'dados.csv'
 
-data = np.genfromtxt(data_file, delimiter=',', dtype=(float, float),
-                     names=['time', 'concentration'], skip_header=1)
 
-time = data['time'] * ureg('min')
-concentration = data['concentration'] * ureg('mole / litre')
+class Kinetics:
+    def __init__(self, data_file, time_unit='min', conc_unit='mole / litre'):
+        self.data_file = data_file
+        self.time = self.data_from_file['time'] * ureg(time_unit)
+        self.concentration = self.data_from_file['concentration'] * ureg(conc_unit)
 
-# zero order
-regression_zero_order = linregress(time.magnitude, concentration.magnitude)
-x = np.linspace(time[0].magnitude, time[-1].magnitude, 2)
-y = regression_zero_order.slope * x + regression_zero_order.intercept
+    @property
+    def data_from_file(self):
+        return np.genfromtxt(self.data_file, delimiter=',', dtype=(float, float),
+                             names=['time', 'concentration'], skip_header=1)
 
-fig, ax = plt.subplots(figsize=(8, 6))
-plot_params(ax)
-ax.plot(x, y, color='red')
-ax.scatter(time, concentration)
-ax.text(0.5, 0.90, f'y = {regression_zero_order.slope:.3e}x + {regression_zero_order.intercept:.3e}',
-        fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
-        transform=ax.transAxes, horizontalalignment='center')
-ax.set_xlabel(f'Time / {time.units:~P}', fontsize=18)
-ax.set_ylabel(f'Concentration / {concentration.units:~P}', fontsize=18)
+    def regression(self, order):
+        if order == 0:
+            reg = linregress(self.time.magnitude, self.concentration.magnitude)
+        elif order == 1:
+            reg = linregress(self.time.magnitude, np.log(self.concentration.magnitude))
+        elif order == 2:
+            reg = linregress(self.time.magnitude, 1/(self.concentration.magnitude))
+        else:
+            raise ValueError('Invalid order')
+        return reg
 
-# first order
-regression_first_order = linregress(time.magnitude, np.log(concentration.magnitude))
-x = np.linspace(time[0].magnitude, time[-1].magnitude, 2)
-y = regression_first_order.slope * x + regression_first_order.intercept
+    def linear_fit(self, order):
+        x = np.linspace(self.time[0].magnitude, self.time[-1].magnitude, 2)
+        y = self.regression(order).slope * x + self.regression(order).intercept
+        return x, y
 
-fig, ax = plt.subplots(figsize=(8, 6))
-plot_params(ax)
-ax.plot(x, y, color='red')
-ax.scatter(time, np.log(concentration.magnitude))
-ax.text(0.5, 0.90, f'y = {regression_first_order.slope:.3e}x + {regression_first_order.intercept:.3e}',
-        fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
-        transform=ax.transAxes, horizontalalignment='center')
-ax.set_xlabel(f'Time / {time.units:~P}', fontsize=18)
-ax.set_ylabel(f'ln(Concentration / {concentration.units:~P})', fontsize=18)
-
-# second order
-regression_second_order = linregress(time.magnitude, 1/(concentration.magnitude))
-x = np.linspace(time[0].magnitude, time[-1].magnitude, 2)
-y = regression_second_order.slope * x + regression_second_order.intercept
-
-fig, ax = plt.subplots(figsize=(8, 6))
-plot_params(ax)
-ax.plot(x, y, color='red')
-ax.scatter(time, 1/(concentration.magnitude))
-ax.text(0.50, 0.90, f'y = {regression_second_order.slope:.3e}x + {regression_second_order.intercept:.3e}',
-        fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
-        transform=ax.transAxes, horizontalalignment='center')
-ax.set_xlabel(f'Time / {time.units:~P}', fontsize=18)
-ax.set_ylabel(f'1/(Concentration / {concentration.units:~P})', fontsize=18)
-plt.show()
+    def plot(self, order):
+        fig, ax = plt.subplots(figsize=(8, 6))
+        plot_params(ax)
+        if order == 0:
+            ax.plot(*self.linear_fit(0), color='red')
+            ax.scatter(self.time, self.concentration)
+            ax.text(0.5, 0.90, f'y = {self.regression(0).slope:.3e}x + {self.regression(0).intercept:.3e}',
+                    fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
+                    transform=ax.transAxes, horizontalalignment='center')
+            ax.set_xlabel(f'Time / {self.time.units:~P}', fontsize=18)
+            ax.set_ylabel(f'Concentration / {self.concentration.units:~P}', fontsize=18)
+        elif order == 1:
+            ax.plot(*self.linear_fit(1), color='red')
+            ax.scatter(self.time, np.log(self.concentration.magnitude))
+            ax.text(0.5, 0.90, f'y = {self.regression(1).slope:.3e}x + {self.regression(1).intercept:.3e}',
+                    fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
+                    transform=ax.transAxes, horizontalalignment='center')
+            ax.set_xlabel(f'Time / {self.time.units:~P}', fontsize=18)
+            ax.set_ylabel(f'ln(Concentration / {self.concentration.units:~P})', fontsize=18)
+        elif order == 2:
+            ax.plot(*self.linear_fit(2), color='red')
+            ax.scatter(self.time, 1/(self.concentration.magnitude))
+            ax.text(0.5, 0.90, f'y = {self.regression(2).slope:.3e}x + {self.regression(2).intercept:.3e}',
+                    fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white',
+                    transform=ax.transAxes, horizontalalignment='center')
+            ax.set_xlabel(f'Time / {self.time.units:~P}', fontsize=18)
+            ax.set_ylabel(f'1/(Concentration / {self.concentration.units:~P})', fontsize=18)
+        plt.show()
