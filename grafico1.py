00c1f66c81d3a8722308ab8aa67dc194ae545e41
Francisco Bustamante <chicolucio@gmail.com>
2021-08-24 16:27:59 -0300

    Primeira versao somente com concentraçao x tempo

--- grafico1.py
+++ grafico1.py
@@ -8,35 +8,14 @@ from helpers import plot_params
 ureg = pint.UnitRegistry()
 ureg.setup_matplotlib()
 
+data_file = 'dados.csv'
 
-def _data_from_file(data_file):
-    return np.genfromtxt((data_file), delimiter=',', dtype=(float, float), names=['tempo / min', 'conc / mol/l'], skip_header=1)
+data = np.genfromtxt(data_file, delimiter=',', dtype=(float, float),
+                     names=['time', 'concentration'], skip_header=1)
 
+time = data['time'] * ureg('min')
+concentration = data['concentration'] * ureg('mole / litre')
 
-class grafico_1:
-
- def __init__(self, tempo, concentraçao, data_file):
-    self.tempo = tempo
-    self.concentraçao = concentraçao
-    self.data = self._data_from_file(data_file)
-
-
-@property
-def tempo(self):
-    return self.tempo['tempo / min'] * ureg('min')
-
-
-@property
-def concentraçao(self):
-    return self.concentraçao['conc / mol/l'] * ureg('mol/l')
-
-
-@property
-def regression(self):
-    tempo_por_concentraçao = self.tempo / self.concentraçao
-    return linregress(self.concentraçao.magnitude, tempo_por_concentraçao.magnitude)
-
-
-def plot(self):
-    fig, ax = plt.subplots(figsize=(8, 6), nrows=1, ncols=1, tight_layout=True)
-    plot_params(ax)
+fig, ax = plt.subplots(figsize=(8, 6))
+ax.scatter(time, concentration)
+plt.show()
