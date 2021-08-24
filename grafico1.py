9d2fbd06cba5c3b988f2c8436ee68c3613aa3a93
Francisco Bustamante <chicolucio@gmail.com>
2021-08-24 16:35:43 -0300

    Codigo com regressao para ordem zero

--- grafico1.py
+++ grafico1.py
@@ -16,6 +16,13 @@ data = np.genfromtxt(data_file, delimiter=',', dtype=(float, float),
 time = data['time'] * ureg('min')
 concentration = data['concentration'] * ureg('mole / litre')
 
+# zero order
+regression_zero_order = linregress(time.magnitude, concentration.magnitude)
+x = np.linspace(time[0].magnitude, time[-1].magnitude, 2)
+y = regression_zero_order.slope * x + regression_zero_order.intercept
+
 fig, ax = plt.subplots(figsize=(8, 6))
+ax.plot(x, y)
 ax.scatter(time, concentration)
+
 plt.show()
