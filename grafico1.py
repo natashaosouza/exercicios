d9279ae8f24145f7618830a6b8ce7c3ae8d19aba
Francisco Bustamante <chicolucio@gmail.com>
2021-08-24 17:02:02 -0300

    Grafico primeira ordem

--- grafico1.py
+++ grafico1.py
@@ -30,4 +30,18 @@ ax.text(7.75, 0.90, f'y = {regression_zero_order.slope:.3e}x + {regression_zero_
 ax.set_xlabel(f'Time / {time.units:~P}', fontsize=18)
 ax.set_ylabel(f'Concentration / {concentration.units:~P}', fontsize=18)
 
+# first order
+regression_first_order = linregress(time.magnitude, np.log(concentration.magnitude))
+x = np.linspace(time[0].magnitude, time[-1].magnitude, 2)
+y = regression_first_order.slope * x + regression_first_order.intercept
+
+fig, ax = plt.subplots(figsize=(8, 6))
+plot_params(ax)
+ax.plot(x, y, color='red')
+ax.scatter(time, np.log(concentration.magnitude))
+ax.text(7.75, 0.00, f'y = {regression_first_order.slope:.3e}x + {regression_first_order.intercept:.3e}',
+        fontsize=14, bbox=dict(facecolor='red', alpha=0.9), color='white')
+ax.set_xlabel(f'Time / {time.units:~P}', fontsize=18)
+ax.set_ylabel(f'ln(Concentration / {concentration.units:~P})', fontsize=18)
+
 plt.show()
